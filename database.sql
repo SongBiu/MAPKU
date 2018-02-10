3bccda00a08e710bd6e416b4f1a0b92d7c165be2
Song862 <32703184+Song862@users.noreply.github.com>
2018-02-10 23:25:44 +0800

    database
    睡前更新，更新了数据库

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE usr(
-	usr_id VARCHAR(10),
+	usr_id VARCHAR(40),
 	community_id VARCHAR(5) DEFAULT NULL,
 	score INT DEFAULT 0,
 	PKU BOOL DEFAULT FALSE,
@@ -16,21 +16,21 @@ CREATE TABLE community (
 
 CREATE TABLE invitate (
 	invitation_code VARCHAR(15),
-	inviter_id VARCHAR(10) NOT NULL,
+	inviter_id VARCHAR(40) NOT NULL,
 	PRIMARY KEY(invitation_code),
 	FOREIGN KEY (inviter_id) REFERENCES usr(usr_id)
 )DEFAULT CHARSET=utf8;
 
 CREATE TABLE invitated (
 	invitation_code VARCHAR(15),
-	newusr_id VARCHAR(10),
+	newusr_id VARCHAR(40),
 	PRIMARY KEY(invitation_code, newusr_id),
 	FOREIGN KEY (newusr_id) REFERENCES usr(usr_id)
 )DEFAULT CHARSET=utf8;
 
 CREATE TABLE dyna (
 	dynamic_id VARCHAR(15),
-	usr_id VARCHAR(10),
+	usr_id VARCHAR(40),
 	dynamic_date DATETIME,
 	good INT DEFAULT 0,
 	count_bag INT,

a2fa71bc99bd12380f0e9e142f4ebdb88faeea1d
SongBiu <song_pku@foxmail.com>
2018-02-11 20:43:45 +0800

    database
    更新了一波数据库

--- database.sql
+++ database.sql
@@ -6,17 +6,23 @@ CREATE TABLE community (
 )DEFAULT CHARSET=utf8;
 
 CREATE TABLE usr(
-	usr_id VARCHAR(40),
-	community_id VARCHAR(5) DEFAULT NULL,
+	usrID VARCHAR(40),
+	communityID VARCHAR(5) DEFAULT NULL,
 	score INT DEFAULT 0,
 	PKU BOOL DEFAULT FALSE,
 	name VARCHAR(30) NOT NULL,
 	email VARCHAR(30) NOT NULL,
-	PRIMARY KEY (usr_id),
-	FOREIGN KEY (community_id) REFERENCES community(community_id)
+	PRIMARY KEY (usrID),
+	FOREIGN KEY (communityID) REFERENCES community(communityID)
 )DEFAULT CHARSET=utf8;
 
-<<<<<<< HEAD
+CREATE TABLE community (
+	communityID VARCHAR(10),
+	name VARCHAR(20),
+	allBag INT DEFAULT 0,
+	PRIMARY KEY (communityID)
+)DEFAULT CHARSET=utf8;
+
 CREATE TABLE invitate (
 	invitateCode VARCHAR(15),
 	usrID VARCHAR(40) NOT NULL,
@@ -27,42 +33,20 @@ CREATE TABLE invitate (
 CREATE TABLE invitated (
 	invitateCode VARCHAR(15),
 	usrID VARCHAR(40),
-	PRIMARY KEY(usrID),
-	FOREIGN KEY (usrID) REFERENCES usr(usrID),
-	FOREIGN KEY (invitateCode) REFERENCES invitate(invitateCode)
-=======
-CREATE TABLE community (
-	community_id VARCHAR(10),
-	name VARCHAR(20),
-	allBag INT DEFAULT 0,
-	PRIMARY KEY (community_id)
-)DEFAULT CHARSET=utf8;
-
-CREATE TABLE invitate (
-	invitation_code VARCHAR(15),
-	inviter_id VARCHAR(40) NOT NULL,
-	PRIMARY KEY(invitation_code),
-	FOREIGN KEY (inviter_id) REFERENCES usr(usr_id)
-)DEFAULT CHARSET=utf8;
-
-CREATE TABLE invitated (
-	invitation_code VARCHAR(15),
-	newusr_id VARCHAR(40),
-	PRIMARY KEY(invitation_code, newusr_id),
-	FOREIGN KEY (newusr_id) REFERENCES usr(usr_id)
->>>>>>> parent of 21af9e4... name
+	PRIMARY KEY(invitateCode, usrID),
+	FOREIGN KEY (usrID) REFERENCES usr(usrID)
 )DEFAULT CHARSET=utf8;
 
 CREATE TABLE dyna (
-	dynamic_id VARCHAR(15),
-	usr_id VARCHAR(40),
-	dynamic_date DATETIME,
+	dynamicID VARCHAR(15),
+	usrID VARCHAR(40),
+	dynamicDate DATETIME,
 	good INT DEFAULT 0,
-	count_bag INT,
+	countBag INT,
 	say VARCHAR(140) DEFAULT NULL,
 	image VARCHAR(20) DEFAULT NULL,
-	PRIMARY KEY(dynamic_id, usr_id),
-	FOREIGN KEY (usr_id) REFERENCES usr(usr_id)
+	PRIMARY KEY(dynamicID, usrID),
+	FOREIGN KEY (usrID) REFERENCES usr(usrID)
 )DEFAULT CHARSET=utf8;
 
 
@@ -70,19 +54,7 @@ SELECT MAX(countBag), name
 FROM community
 GROUP by name
 
-<<<<<<< HEAD
 INSERT INTO dyna(dynamicID, usrID, dynamicDate, countBag, say) VALUES ('0')
 
 北大山鹰社
 李松
-=======
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('0', '5', '2018-1-1 12:12:12', 1, 'today I am happy', 'x.png');
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('1', '5', '2017-1-1 12:12:12', 1, 'hi, I am lisong', 'x.png');
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('2', '5', '2017-1-1 12:12:12', 1, 'hi, I am lisong', 'x.png');
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('3', '5', '2017-1-1 12:12:12', 1, 'hi, I am lisong', 'x.png');
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('4', '5', '2017-1-1 12:12:12', 1, 'hi, I am lisong', 'x.png');
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('5', '5', '2017-1-1 12:12:12', 1, 'hi, I am lisong', 'x.png');
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('6', '5', '2017-1-1 12:12:12', 1, 'hi, I am lisong', 'x.png');
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('7', '5', '2017-1-1 12:12:12', 1, 'hi, I am lisong', 'x.png');
-INSERT INTO dyna(dynamic_id, usr_id, dynamic_date, count_Bag, say, image) VALUES ('8', '5', '2017-1-1 12:12:12', 1, 'hi, I am lisong', 'x.png');
->>>>>>> parent of 21af9e4... name

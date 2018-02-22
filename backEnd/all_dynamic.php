7511d4905423c3e75839c1be669d1f0759c8edbf
SongBiu <song_pku@foxmail.com>
2018-02-22 18:19:15 +0800

    OK
    隐私错误已经修补

---
+++ backEnd/all_dynamic.php
@@ -0,0 +1,42 @@
+<?php
+	include "connect.php";
+	$conn = connect();
+	if (!$conn) {
+		die('error');
+		exit;
+	}
+	$sql = "SELECT * FROM dyna ORDER BY dynamicDate DESC";
+	mysqli_query($conn, "SET NAMES utf8");
+	$rslt = mysqli_query($conn, $sql);
+	if (!$rslt) {
+		die("error");
+		exit;
+	}
+	$index = 0;
+	$data = Array();
+	while ($row = mysqli_fetch_assoc($rslt)) {
+		$dyna = Array();
+		$dyna['say'] = $row["say"];
+		$dyna["countBag"] = $row['countBag'];
+		$dyna["good"] = $row["good"];
+		$dyna['time'] = $row['dynamicDate'];
+		if ($row['image'] == NULL) {
+			$dyna['hasImg'] = false;
+		}
+		else {
+			$dyna['hasImg'] = true;
+		}
+		$dyna['image'] = 'img/'.$row['image'];
+		$dyna['dynamicID'] = $row['dynamicID']; 
+		$usrID = $row['usrID'];
+		$sql = "SELECT name FROM usr WHERE usrID = '" . $usrID . "'";
+		$r = mysqli_query($conn, $sql);
+		$name = '';
+		while ($row = mysqli_fetch_assoc($r)) {
+			$name = $row['name'];
+		}
+		$dyna['name'] = $name;
+		$data[$index++] = $dyna;
+	}
+	print(json_encode($data));
+?>

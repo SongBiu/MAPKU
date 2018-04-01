f6313077990001a4271cd9e1bfdb4ae81018c653
SongBiu <song_pku@foxmail.com>
2018-04-01 19:27:56 +0800

    remove
    删除了一些输出

--- app.js
+++ app.js
@@ -31,17 +31,17 @@ App({
 		//获取用户信息
 		wx.getSetting({
 			success: res => {
-				console.log(res)
+				
 				if (res.authSetting['scope.userInfo']) {
 					// 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-					// console.log("ok")
+					
 					wx.getUserInfo({
 						success: res => {
 							// this.getUsetInfo(res)
-							console.log("up ok")
+							
 							// 可以将 res 发送给后台解码出 unionId
 							this.globalData.userInfo = res.userInfo
-							console.log(res)
+							
 							// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
 							// 所以此处加入 callback 以防止这种情况
 							if (this.userInfoReadyCallback) {
@@ -54,10 +54,8 @@ App({
 					wx.getUserInfo({
 						success: res => {
 
-							console.log("up ok")
 							// 可以将 res 发送给后台解码出 unionId
 							this.globalData.userInfo = res.userInfo
-							console.log(res)
 							// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
 							// 所以此处加入 callback 以防止这种情况
 							if (this.userInfoReadyCallback) {

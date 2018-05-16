418d31eb45447d8702994ef86d39c41fb5080d24
SongBiu <song_pku@foxmail.com>
2018-05-16 23:45:31 +0800

    upload
    upload ok

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -1,49 +1,102 @@
 var app = getApp();
 Page({
 	data: {
-		imgPath:null,
-		openid:null,
-		app:getApp()
+		img_url: null,
+		say: "",
+		types: ["顺手一袋", "心情记录"],
+		position: "",
+		count_bag: 0,
+		index: 0,
+		say_length: 0
 	},
-	
-	formSubmit: function(event) {
-		var img = app.imgPath;
-		var value = event.detail.value;
-		var that = this;
-		wx.request({
-			url: app.url_pre + '/upload.php',
-			header: {
-				"Content-Type": "application/x-www-form-urlencoded"
-			},
-			method: 'POST',
-			data: {
-				countBag: event.detail.value.number,
-				say: event.detail.value.talk,
-				usrID:this.data.openid
-			},
-			success: function(res) {
-				console.log(res)
-				wx.uploadFile({
-					url: app.url_pre + '/uploadImage.php',
-					filePath: img,
-					name: 'image',
-					formData: {
-						usrID: that.data.openid,
-						dynamicID:res.data.dynamicID
-					},
-					complete: function (res) {
-						
-					}
-				})
-			}
-		})
-		wx.redirectTo({
-			url:"../index/index"
-		})
+	bind_input: function (e) {
+		console.log(e)
+		if (this.data.say.length < 140) {
+			var say = e.detail.value;
+			this.setData({
+				say: say,
+				say_length: say.length
+			})
+		}
 	},
-	onLoad:function() {
+	bindPicker: function (e) {
+		console.log(e)
+		var index = parseInt(e.detail.value);
 		this.setData({
-			openid:app.openid
+			index: index
+		})
+	},
+	choose_position: function (e) {
+		var that = this;
+		wx.chooseLocation({
+			success: function (res) {
+				console.log(res)
+				if (res.name == null) {
+					that.setData({
+						position: res.address
+					})
+				} else if (res.address == null) {
+					that.setData({
+						position: res.name
+					})
+				} else if (res.name == null && res.address == null) {
+					that.setData({
+						position: "获取地址失败"
+					})
+				} else {
+					that.setData({
+						position: res.name + ', ' + res.address
+					})
+				}
+
+			},
+		})
+	},
+	hideLocal: function () {
+		this.setData({
+			position: ""
+		})
+	},
+	choose_img: function () {
+		var that = this;
+		wx.chooseImage({
+			count: 1,
+			success: function (res) {
+				that.setData({
+					img_url: res.tempFilePaths[0]
+				})
+			},
+		})
+	},
+	bind_slider: function (e) {
+		var count_bag = e.detail.value;
+		this.setData({
+			count_bag: count_bag
+		})
+	},
+	submit: function () {
+		var that = this;
+		wx.uploadFile({
+			url: 'http://39.106.71.227/upload',
+			header: {
+				"Content-Type": "multipart/form-data",
+				"cookie": wx.getStorageSync('cookie')
+			},
+			filePath: that.data.img_url,
+			method: 'POST',
+			name: 'image',
+			formData: {
+				say: that.data.say,
+				position: that.data.position,
+				count_bag: that.data.count_bag,
+				index: that.data.index
+			},
+			success: function (res) {
+				
+			},
+			fail: function (res) {
+				console.log(res)
+			}
 		})
 	}
 })
